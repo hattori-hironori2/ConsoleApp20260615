6acd2307b5e45f2f936d67755835ea1422a7d28f
Taro <taro@example.com>
2026-06-15 14:11:15 +0900

    Initial commit

---
+++ ConsoleApp20260615/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        ' コンソールに文字列を表示する
+        Console.WriteLine("Hello World")
+
+        ' 実行直後にウィンドウが閉じるのを防ぐ（必要に応じて追加）
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ConsoleApp20260615/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("ConsoleApp20260615")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleApp20260615")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("d256f821-7ef3-4a7f-aadf-c805f2a342be")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
